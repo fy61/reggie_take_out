c5bf623d55885dfbd4ec1a4c94f8805d3420308d
hellowd <hellowd@localhost>
2023-12-15 13:22:03 +0800

    Jackson

--- src/main/java/com/lyf/reggie/utils/SMSUtils.java
+++ src/main/java/com/lyf/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tQwqA5DwXxLCJYytKpE", "KNLmUtB8FrSP95tmxCCdo4faXziPFO");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
